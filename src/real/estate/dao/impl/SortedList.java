4c5d76c2ca19e0576b8fe4880e7dcaba465eaae3
NisansalaWijesundara <nisansalawijesundara@gmail.com>
2015-03-15 18:07:38 +0530

    Added swap method to SortedList class

--- src/real/estate/dao/impl/SortedList.java
+++ src/real/estate/dao/impl/SortedList.java
@@ -3,35 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package real.estate.dao.impl;
-import real.estate.dao .*;
+
+import real.estate.dao.*;
+
 /**
  *
- * @author Chathu
+ *
  */
 public class SortedList {
-    public void sort(int list[]){
-      for(int i = list.length - 1; i > 1; i--){
-			
-			
-			
-			for(int j = 0; j < i; j++){
-				
-				if(list[j] > list[j + 1]){
-					
-				swap(j, j+1);
-					
-				}
-				
-			}
-			
-		}
-		
-	}
-    public void swap(int i,int j){
-        
-    }
+
+    public void sort(int list[]) {
+        for (int i = list.length - 1; i > 1; i--) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (list[j] > list[j + 1]) {
+
+                    swap(j, j + 1, list);
+
+                }
+
+            }
+
+        }
 
     }
 
+    public void swap(int i, int j, int list[]) {
+        int temp = 0;
+        list[temp] = list[j + 1];
+        list[j + 1] = list[i];
+        list[i] = list[temp];
+
+    }
+
+}
