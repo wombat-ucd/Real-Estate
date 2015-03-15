366c086fe09092309962ed0a0dbbccb54e455f2c
NisansalaWijesundara <nisansalawijesundara@gmail.com>
2015-03-15 18:05:15 +0530

    Added sort method  to SortedList class

--- src/real/estate/dao/impl/SortedList.java
+++ src/real/estate/dao/impl/SortedList.java
@@ -5,11 +5,33 @@
  */
 
 package real.estate.dao.impl;
-
+import real.estate.dao .*;
 /**
  *
  * @author Chathu
  */
 public class SortedList {
-    
-}
+    public void sort(int list[]){
+      for(int i = list.length - 1; i > 1; i--){
+			
+			
+			
+			for(int j = 0; j < i; j++){
+				
+				if(list[j] > list[j + 1]){
+					
+				swap(j, j+1);
+					
+				}
+				
+			}
+			
+		}
+		
+	}
+    public void swap(int i,int j){
+        
+    }
+
+    }
+
