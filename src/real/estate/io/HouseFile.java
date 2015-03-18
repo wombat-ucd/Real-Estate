cf52a485b27474b4d259a9f64c16bb6a8d55b162
Dineesha <dinsuraw@gmail.com>
2015-03-18 11:57:19 +0530

    created file in HouseFile class

--- src/real/estate/io/HouseFile.java
+++ src/real/estate/io/HouseFile.java
@@ -6,10 +6,49 @@
 
 package real.estate.io;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author Chathu
  */
 public class HouseFile {
+
+private static BufferedReader inFile;
+private static PrintWriter outFile;
+private static boolean inFileOpen = false;
+private static boolean outFileOpen = false;
+private static String inString ="";
     
+public static void reset() throws IOException{
+    if(inFileOpen){
+        inFile.close();
+    }
+    if(outFileOpen){
+        outFile.close();
+    }
+    inFile = new BufferedReader(new FileReader("house_details.dat"));
+    inFileOpen = true;
+    inString = inFile.readLine();
+}
+public static void resetForWrite() throws IOException{
+    if(inFileOpen){
+        inFile.close();
+    }
+    if(outFileOpen){
+        outFile.close();
+    }
+    outFile = new PrintWriter(new FileWriter("house_details.dat"));
+    outFileOpen = true;
+}
+
+public static void main(String []args) throws IOException{
+    HouseFile hs=new HouseFile();
+    hs.reset();
+    hs.resetForWrite();
+    }
 }
