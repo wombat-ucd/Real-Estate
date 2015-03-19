c0ed6dee71f90941f7a81635c64b90c418dcee6f
Dineesha <dinsuraw@gmail.com>
2015-03-19 13:13:55 +0530

    created isMoreHousesExist and getNextHouse methods
    added 2 methods to HouseFile class

--- src/real/estate/io/HouseFile.java
+++ src/real/estate/io/HouseFile.java
@@ -11,6 +11,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import real.estate.dao.ListHouse;
 
 /**
  *
@@ -22,7 +23,7 @@ private static BufferedReader inFile;
 private static PrintWriter outFile;
 private static boolean inFileOpen = false;
 private static boolean outFileOpen = false;
-private static String inString ="";
+private static String stringIn ="";
     
 public static void reset() throws IOException{
     if(inFileOpen){
@@ -33,7 +34,7 @@ public static void reset() throws IOException{
     }
     inFile = new BufferedReader(new FileReader("house_details.dat"));
     inFileOpen = true;
-    inString = inFile.readLine();
+    stringIn = inFile.readLine();
 }
 public static void resetForWrite() throws IOException{
     if(inFileOpen){
@@ -45,10 +46,47 @@ public static void resetForWrite() throws IOException{
     outFile = new PrintWriter(new FileWriter("house_details.dat"));
     outFileOpen = true;
 }
+public static boolean isMoreHousesExist(){
+    return stringIn != null;
+   
+    }
+public static void putDataToFile(){
+    
+}
+public static ListHouse getNextHouse() throws IOException{
+    String lastName;
+    String firstName;
+    int lotNo;
+    int price;
+    int squareFeet;
+    int noOfBedRooms;
+    
+    lastName=stringIn;
+    firstName=inFile.readLine();
+    lotNo=Integer.parseInt(inFile.readLine());
+    price=Integer.parseInt(inFile.readLine());
+    squareFeet=Integer.parseInt(inFile.readLine());
+    noOfBedRooms=Integer.parseInt(inFile.readLine());
+    
+    stringIn=inFile.readLine();
+    
+    ListHouse hs1=new ListHouse(lastName,firstName,lotNo,price,squareFeet,noOfBedRooms);
+    System.out.println("correct");
+    return hs1;
+}
+public static void closeFile() throws IOException{
+    if(inFileOpen)
+        inFile.close();
+    if(outFileOpen)
+        outFile.close();
+}
 
 public static void main(String []args) throws IOException{
     HouseFile hs=new HouseFile();
     hs.reset();
     hs.resetForWrite();
+    hs.isMoreHousesExist();
+   // hs.getNextHouse();
     }
+
 }
