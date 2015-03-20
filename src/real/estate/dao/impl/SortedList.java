b6cb45726e4282e807d68a921e7836f7aa4a671c
Chathu Vishwajith <4evercreations@gmail.com>
2015-03-20 18:04:18 +0530

    Fix Sorted List

--- src/real/estate/dao/impl/SortedList.java
+++ src/real/estate/dao/impl/SortedList.java
@@ -13,70 +13,120 @@ import real.estate.dao.*;
  */
 public class SortedList extends List {
 
+    // Instantiates and returns a reference to an empty list object 
+    // with room for maxItems elements
     public SortedList(int maxItems) {
         super(maxItems);
     }
 
-    public void sort(int list[]) {
-        for (int i = list.length - 1; i > 1; i--) {
-
-            for (int j = 0; j < i; j++) {
-
-                if (list[j] > list[j + 1]) {
-
-                    swap(j, j + 1, list);
-
-                }
-
-            }
-
-        }
-
+    // Instantiates and returns a reference to an empty list object 
+    // with room for 100 elements 
+    public SortedList() {
+        super(100);
     }
 
-    public void swap(int i, int j, int list[]) {
-        int temp = 0;
-        list[temp] = list[j + 1];
-        list[j + 1] = list[i];
-        list[i] = list[temp];
-
-    }
-
-    @Override
+    // Returns true if an element with the same key as item is on this list; 
+    // otherwise, returns false 
     public boolean isThere(Listable item) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //Remove above line and implement the code
-    }
+        int compareResult;
+        int midPoint;
+        int first = 0;
+        int last = num_of_items - 1;
+        boolean moreToSearch = (first <= last);
+        boolean found = false;
 
-    @Override
-    public Listable retrieve(Listable item) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //Remove above line and implement the code
-    }
+        while (moreToSearch && !found) {
+            midPoint = (first + last) / 2;
+            compareResult = item.compareTo(list[midPoint]);
+            if (compareResult == 0) {
+                found = true;
+            } else if (compareResult < 0) // item is less than element at location
+            {
+                last = midPoint - 1;
+                moreToSearch = (first <= last);
+            } else // item is greater than element at location 
+            {
+                first = midPoint + 1;
+                moreToSearch = (first <= last);
+            }
+            {
 
-    @Override
-    public void insert(Listable item) {
-        boolean search = (position < num_of_items);
-        while (search) {
-            if (item.compareTo(list[position]) < 0) {
-                search = false;
-            } else {
-                position++;
-                search = (position < num_of_items);
             }
         }
-        for (int index = num_of_items; index > position; index--) {
-            list[index] = list[index - 1];
+        return found;
+    }
+
+    // Returns a copy of the list element with the same key as item 
+    public Listable retrieve(Listable item) {
+        int compareResult;
+
+        int first = 0;
+        int last = num_of_items - 1;
+        int midPoint = (first + last) / 2;
+        boolean found = false;
+        while (!found) {
+            midPoint = (first + last) / 2;
+            compareResult = item.compareTo(list[midPoint]);
+            if (compareResult == 0) {
+                found = true;
+            } else if (compareResult < 0) // item is less than element at location 
+            {
+                last = midPoint - 1;
+            } else // item is greater than element at location 
+            {
+                first = midPoint + 1;
+            }
+
         }
-        list[position] = item.copy();
+        return list[midPoint].copy();
+    }
+
+    public void insert(Listable item) // Adds a copy of item to this list 
+    {
+        int location = 0;
+        boolean moreToSearch = (location < num_of_items);
+        while (moreToSearch) {
+            if (item.compareTo(list[location]) < 0) // item is less 
+            {
+                moreToSearch = false;
+            } else // item is more 
+            {
+                location++;
+                moreToSearch = (location < num_of_items);
+            }
+        }
+        {
+            for (int index = num_of_items; index > location; index--) {
+                list[index] = list[index - 1];
+            }
+        }
+        list[location] = item.copy();
         num_of_items++;
     }
+    
+    // Deletes the element that matches item from this list 
+    public void delete(Listable item) 
+    {
+        int location = 0;
 
-    @Override
-    public void delete(Listable item) {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //Remove above line and implement the code
+        while (item.compareTo(list[location]) != 0) {
+            location++;
+        }
+        for (int index = location + 1;
+                index < num_of_items;
+                index++) {
+            list[index - 1] = list[index];
+        }
+        num_of_items--;
     }
 
+    @Override
+    public int compareTo(Listable other) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
+    @Override
+    public Listable copy() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
