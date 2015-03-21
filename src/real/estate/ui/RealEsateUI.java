9974901e8d088dc719ac118436913c86e0dbccfe
Subhashi Palika <subhashipalika@gmail.com>
2015-03-21 15:53:58 +0530

    add clearHouse method to the UI

--- src/real/estate/ui/RealEsateUI.java
+++ src/real/estate/ui/RealEsateUI.java
@@ -48,6 +48,15 @@ public class RealEsateUI extends javax.swing.JFrame {
         return house;
     }
 
+    private void clearHouse() {
+        txtLotNumber.setText("");
+        txtFirstName.setText("");
+        txtFirstName.setText("");
+        txtPrice.setText("");
+        txtSqFt.setText("");
+        txtNumberOfBedrooms.setText("");
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
