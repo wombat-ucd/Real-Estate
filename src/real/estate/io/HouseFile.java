a6ad8c389999d1a32898350273895af99fdd49d9
Chathu Vishwajith <4evercreations@gmail.com>
2015-03-26 23:12:04 +0530

    Add read and write methods

--- src/real/estate/io/HouseFile.java
+++ src/real/estate/io/HouseFile.java
@@ -6,12 +6,8 @@
 
 package real.estate.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import real.estate.dao.ListHouse;
+import real.estate.dao.impl.SortedList;
+
 
 /**
  *
@@ -19,81 +15,12 @@ import real.estate.dao.ListHouse;
  */
 public class HouseFile {
 
-private static BufferedReader inFile;
-private static PrintWriter outFile;
-private static boolean isInFileOpen = false;
-private static boolean isOutFileOpen = false;
-private static String stringInput ="";
     
-public static void reset() throws IOException{
-    if(isInFileOpen){
-        inFile.close();
-    }
-    if(isOutFileOpen){
-        outFile.close();
-    }
-    inFile = new BufferedReader(new FileReader("house_details.dat"));
-    isInFileOpen = true;
-    stringInput = inFile.readLine();
+public static SortedList readJSONFileToList(){
+    throw new UnsupportedOperationException();
 }
-public static void resetForWrite() throws IOException{
-    if(isInFileOpen){
-        inFile.close();
-    }
-    if(isOutFileOpen){
-        outFile.close();
-    }
-    outFile = new PrintWriter(new FileWriter("house_details.dat"));
-    isOutFileOpen = true;
-}
-public static boolean isMoreHousesExist(){
-    return stringInput != null;
-   
-    }
-public static void putDataToFile(ListHouse houseItem){
-    outFile.println(houseItem.firstName());
-    outFile.println(houseItem.lastName());
-    outFile.println(houseItem.lotNumber());
-    outFile.println(houseItem.price());
-    outFile.println(houseItem.squareFeet());
-    outFile.println(houseItem.numberOfBedRooms());
+public static void writeJSONToFile(){
     
 }
-public static ListHouse getNextHouseItem() throws IOException{
-    String lastName;
-    String firstName;
-    int lotNo;
-    int price;
-    int squareFeet;
-    int noOfBedRooms;
-    
-    lastName=stringInput;
-    firstName=inFile.readLine();
-    lotNo=Integer.parseInt(inFile.readLine());
-    price=Integer.parseInt(inFile.readLine());
-    squareFeet=Integer.parseInt(inFile.readLine());
-    noOfBedRooms=Integer.parseInt(inFile.readLine());
-    
-    stringInput=inFile.readLine();
-    
-    ListHouse hs1=new ListHouse(lastName,firstName,lotNo,price,squareFeet,noOfBedRooms);
-    System.out.println("correct");
-    return hs1;
-}
-public static void closeFile() throws IOException{
-    if(isInFileOpen)
-        inFile.close();
-    if(isOutFileOpen)
-        outFile.close();
-}
-
-public static void main(String []args) throws IOException{
-    HouseFile hs=new HouseFile();
-    hs.reset();
-    hs.resetForWrite();
-    hs.isMoreHousesExist();
-   // hs.getNextHouseItem();
-    
-    }
 
 }
