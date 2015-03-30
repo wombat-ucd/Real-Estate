1417c039c0549d7ab3730ebb21ada09a5aa8398d
Chathu Vishwajith <4evercreations@gmail.com>
2015-03-30 11:54:02 +0530

    Add Web Look and Feel

--- src/real/estate/ui/RealEsateUI.java
+++ src/real/estate/ui/RealEsateUI.java
@@ -5,6 +5,7 @@
  */
 package real.estate.ui;
 
+import com.alee.laf.WebLookAndFeel;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -313,32 +314,10 @@ public class RealEsateUI extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RealEsateUI.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RealEsateUI.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RealEsateUI.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RealEsateUI.class
-                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
+        /* Set the Web look and feel */
+        //<editor-fold defaultstate="collapsed" desc="Web Look and feel setting code ">
+        
+         WebLookAndFeel.install ();
         //</editor-fold>
 
         /* Create and display the form */
